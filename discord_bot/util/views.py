83308d6796ed78e19a1d32e5ec6be0e8a502103d
kenzoengineer <kenzoengineer@gmail.com>
2025-12-29 18:39:25 -0500

    print csv for easier pr

--- discord_bot/util/views.py
+++ discord_bot/util/views.py
@@ -3,6 +3,17 @@ from discord.ext import commands
 
 from util.embeds import Embeds
 
+# ya this is duplicated in commands but whatever
+NAMES = ["Souren", "Kevin", "Ken", "Artom", "Andrew"]
+IDS = [
+    "290550631591182336",
+    "183383346569543681",
+    "290630197458501633",
+    "161921661346643968",
+    "196069761082327041",
+]
+ID_NAMES_MAP = {IDS[i]: NAMES[i] for i in range(len(NAMES))}
+
 
 class JudgeView(discord.ui.View):
     def __init__(self, songs: dict[str, str], userIds: list[str]):
@@ -25,4 +36,17 @@ class SongButton(discord.ui.Button):
             )
         )
         await interaction.channel.send(content=f"{self.songs[self.index]}")
+        # generate 4 lines of csv as a string to be added to the winners.csv file
+        # csv should have the format submitter_name, song_link, date YYYY-MM-DD, winner (0 or 1)
+        csv_string = ""
+        from datetime import datetime
+
+        today = datetime.now().date()
+        for key in self.songs:
+            name = ID_NAMES_MAP[str(key)] if str(key) in ID_NAMES_MAP else "Unknown"
+            if key == self.index:
+                csv_string += f"{name},{self.songs[key]},{today},1\n"
+            else:
+                csv_string += f"{name},{self.songs[key]},{today},0\n"
+        await interaction.channel.send(content=f"```csv\n{csv_string}```")
         self.view.stop()
